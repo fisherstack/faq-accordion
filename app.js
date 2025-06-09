c71df5ebd58a6545b4f2d40089c8978ec5b22cc0
Gilles Thomas <githoweb@gmail.com>
2025-06-09 16:09:56 +0200

    second version : only one button, which src is handled by js

--- app.js
+++ app.js
@@ -4,24 +4,18 @@ btns.forEach((btn) => {
   btn.addEventListener("click", (e) => {
     console.log(e);
     const cible = e.currentTarget;
+    const img = cible.querySelector('img');
+
     const faqItem = cible.closest('.faq-item');
-    console.log(faqItem);
+    console.log("img before click = ", img);
     faqItem.classList.toggle("active");
-/*
-    if (faqItem.classList.contains("active")) {
-        element.classList.add("hide");
-        element.classList.remove("active");
-      });
-      cible.classList.remove("hide");
-      cible.classList.add("active");
+
+    if(faqItem.classList.contains("active")) {
+        img.setAttribute("src", "assets/images/icon-minus.svg")
     } else {
-      siblings.forEach((element) => {
-        element.classList.remove("hide");
-        element.classList.remove("active");
-      });
-      cible.classList.add("hide");
-      cible.classList.remove("active");
+        img.setAttribute("src", "assets/images/icon-plus.svg")
+
     }
-      */
+        console.log("img after click = ", img);
   });
 });
